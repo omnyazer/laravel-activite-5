ebe0d04d7e99f2f982b6cbcc9434a614c23c1805
omnya zerghouni <omnyazer@gmail.com>
2026-01-13 21:29:34 +0100

    activité 5

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+
+class LogRequestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userAgent = $request->header('User-Agent');
+
+        Log::info('User-Agent:', [
+            'browser' => $userAgent,
+        ]);
+
+        return $next($request);
+    }
+
+}
